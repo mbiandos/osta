9ca3328b32845ffd6c3842225f0fc6c41b2da430
mbiandos <58226680+mbiandos@users.noreply.github.com>
2022-06-12 17:42:46 +0100

    others tests

---
+++ src/main/scala/apps_streaming.scala
@@ -0,0 +1,13 @@
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.scala.kstream.KStream
+import org.apache.kafka.streams.scala.kstream.KTable
+import org.apache.kafka.streams.scala.kstream.KGroupedStream
+import org.apache.kafka.streams.scala.kstream.KGroupedTable
+import org.apache.kafka.streams.scala.kstream.SessionWindowedKStream
+import org.apache.kafka.streams.scala.kstream.TimeWindowedKStream
+import org.apache.kafka.streams.scala.ImplicitConversions
+import org.apache.kafka.streams.scala.Serdes
+
+object apps_streaming {
+
+}
